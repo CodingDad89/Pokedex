bbda36d16e3a2b79d375143d0eabd2f400955ecb
CodingDad89 <lutz.amgarten@web.de>
2025-04-16 10:09:17 +0200

    fetch function rebuild

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
 let limit = 20;
 let offset = 0;
-let pokeUrl = "https://pokeapi.co/api/v2/pokemon";
+let pokeUrl = `https://pokeapi.co/api/v2/pokemon`;
 let pokemonDb = "";
+let pokemonAbilities = "";
 let pokemonStats = "";
+let pokemonInfosDb = "";
 
-
+function init() {
+    fetchDataPokemon();
+}
 
 
 async function fetchDataPokemon() {
-    let response = await fetch(`${pokeUrl}??limit=${limit}&offset=${offset}`);
-    pokemonDb = await response.json();
-    console.log(pokemonDb);
-    loopPokemon(pokemonDb);
-}
-
-async function fetchDataPokemonInfos() {
-    let response = await fetch(`${pokeUrl}`);
-    pokemonInfosDb = await response.json();
-    console.log(pokemonInfosDb);
-}
-
-function loopPokemon(pokemonDb, i) {
-    for (let i = 0; i < pokemonDb.results.length; i++) {
-        const element = pokemonDb.results[i];
+    for (let i = 1; i <= 20; i++){
+        let response = await fetch(`${pokeUrl}/${i}`);
+        pokemonDb = await response.json();
         let mainContent = document.getElementById('content');
-        mainContent.innerHTML += renderPokecards(i);
+        mainContent.innerHTML += renderPokecards(i);  
     }
 }
 
-function loopPokemonInfos(pokemonInfosDb, i) {
-    for (let j = 1; j < pokemonInfosDb.length; j++) {
-        const element = pokemonInfosDb[j];
-    }
-}
 
 function renderPokecards(i) {
     return  `<div class="pokecard">
     <div class="pokeball" id="pokemon_id${i}">
         <img src="img/pokeball.png" id="pokeball_img">
         <p>#</p>
-         ${pokemonDb.results[i].name}
+         ${pokemonDb.species.name}
     </div>
-    <div>
-        gif 
+        <div>
+    
     </div>
     <div>
         
